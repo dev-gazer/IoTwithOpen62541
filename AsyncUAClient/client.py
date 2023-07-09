ac0af8b03d3a7fb15e3594dde386602cba929684
dev-gazer <martuscellifaria@gmail.com>
2023-07-09 12:34:40 +0200

    Replaced german by english

--- AsyncUAClient/client.py
+++ AsyncUAClient/client.py
@@ -26,7 +26,7 @@ namespace = "http://iot.opcualex.de"
 async def main():
     print(f"Connecting to {url} ...")
     time.sleep(20)
-    send_msg("Ich bin online für dich.")
+    send_msg("I am online for you!.")
     async with Client(url=url) as client:
         nsidx = await client.get_namespace_index(namespace)
         last_replied = 0
@@ -35,13 +35,13 @@ async def main():
                 response = get_last_message()
                 res = response["message"]["text"].lower()
                 if int(time.time()) - response["message"]["date"] < 25 and response["message"]["message_id"] != last_replied:
-                    if res in ["hi", "hallo"]:
-                        send_msg("Hi, alles gut? Was willst du wissen? Ich kann jetzt gerade nur die Temperatur ausmessen.")
-                    elif  res == "temperatur":
+                    if res in ["hi", "hello"]:
+                        send_msg("Hi, how are you? I can only inform you about the temperature. Please send me the 'temperature' command.")
+                    elif  res == "temperature":
                         res = await client.nodes.objects.call_method(f"{nsidx}:get temperature")
-                        send_msg(f"Die Temperatur ist gerade {res.strip('0')}0°C bei euch.")
+                        send_msg(f"Your living room temperature is {res.strip('0')}0°C.")
                     else:
-                        send_msg("Ich habe es nicht verstanden!")
+                        send_msg("I didn't understand! Please send hi.")
                     last_replied = response["message"]["message_id"]
                 time.sleep(5)
             except Exception as err:
